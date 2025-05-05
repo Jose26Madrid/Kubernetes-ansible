084ef27bf9216f4c023be27990b36805c6875df7
sr_ramiro@hotmail.es <sr_ramiro@hotmail.es>
2025-05-05 16:45:13 +0200

    Conf readme y scripts.

--- terraform/main.tf
+++ terraform/main.tf
@@ -72,8 +72,3 @@ resource "aws_instance" "k8s_instance" {
     Name = "k8s-node"
   }
 }
-
-output "ec2_public_ip" {
-  value       = aws_instance.k8s_instance.public_ip
-  description = "IP pública de la instancia EC2 con Kubernetes"
-}
